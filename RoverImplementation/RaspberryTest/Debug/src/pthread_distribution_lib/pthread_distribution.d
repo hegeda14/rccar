aa30743a19a4dc2d5e9215676e0b5132c4ba8ac3
Robert Hoettger <robert.hoettger@fh-dortmund.de>
2017-04-26 15:00:34 +0200

    removed cpp rover sources since they are now available at https://git.eclipse.org/r/app4mc/org.eclipse.app4mc.examples

--- RoverImplementation/RaspberryTest/Debug/src/pthread_distribution_lib/pthread_distribution.d
+++
@@ -1,5 +0,0 @@
-src/pthread_distribution_lib/pthread_distribution.o: \
- ../src/pthread_distribution_lib/pthread_distribution.cpp \
- ../src/pthread_distribution_lib/pthread_distribution.h
-
-../src/pthread_distribution_lib/pthread_distribution.h:
